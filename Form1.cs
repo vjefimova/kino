7450c704d52f2529533a44f4e00443c7cd3e883e
Opilane TTHK <opilane@tthk.ee>
2021-02-04 10:52:37 +0200

    kino

---
+++ Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kino
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Label[,] _arr = new Label[4, 4];
+        Label[] read = new Label[4];
+        Button osta;
+        
+        private void Osta_Click(object sender, EventArgs e)
+        {
+            var vastus = MessageBox.Show("Kas oled kindel?",
+                "Appolo küsib", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (vastus == DialogResult.Yes)
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    for (int j = 0; j < 4; j++)
+                    {
+                        if (_arr[i, j].Image == Image.FromFile("../../images/vibrano.png"))
+                        {
+                            _arr[i, j].Image = Image.FromFile("../../images/zaneto.png");
+                        }
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    for (int j = 0; j < 4; j++)
+                    {
+                        if (_arr[i, j].Image == Image.FromFile("../../images/vibrano.png"))
+                        {
+                            _arr[i, j].Text = " Koht" + (j + 1);
+                            _arr[i, j].Image = Image.FromFile("../../images/free2.png");
+                        }
+                    }
+                }
+            }
+
+        }
+
+        void Form1_Click(object sender, EventArgs e)
+        {
+            var label = (Label)sender;//запомникли на какую надпись нажали
+            var tag = (int[])label.Tag;//определили координаты надписи
+
+            if (_arr[tag[0], tag[1]].Text != "Kinni")
+            {
+                _arr[tag[0], tag[1]].Text = "Kinni";
+                _arr[tag[0], tag[1]].Image = Image.FromFile("../../images/vibrano.png");
+            }
+            else
+            {
+                MessageBox.Show("Koht " + (tag[0] + 1) + (tag[1] + 1) + " juba ostetud!");
+            }
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                read[i] = new Label();
+                read[i].Text = "Rida " + (i + 1);
+                read[i].Size = new Size(100, 100);
+                read[i].Location = new Point(1, i * 100);
+                this.Controls.Add(read[i]);
+                for (int j = 0; j < 4; j++)
+                {
+                    _arr[i, j] = new Label();
+                    _arr[i, j].Text = " Koht" + (j + 1);//"Rida " + i +
+                    _arr[i, j].Size = new Size(100,100);
+                    _arr[i, j].Image = Image.FromFile("../../images/free2.png");
+                    _arr[i, j].BorderStyle = BorderStyle.Fixed3D;
+                    _arr[i, j].Location = new Point(j * 100 + 100, i * 100);
+                    this.Controls.Add(_arr[i, j]);
+                    _arr[i, j].Tag = new int[] { i, j };
+                    _arr[i, j].Click += new System.EventHandler(Form1_Click);
+                }
+            }
+            osta = new Button();
+            osta.Text = "Osta";
+            osta.Location = new Point(500, 200);
+            this.Controls.Add(osta);
+            osta.Click += Osta_Click;
+        }
+    }
+}
---
+++ mainForm.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kino
+{
+    public partial class mainForm : Form
+    {
+        Button[] pictureBTN = new Button[3];
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void mainForm_Load(object sender, EventArgs e)
+        {
+            this.Size = new Size(970,500);
+            for (int i = 0; i < 3; i++)
+            {
+                pictureBTN[i] = new Button();
+                pictureBTN[i].Size = new Size(250, 300);
+                if(i == 0)
+                {
+                    pictureBTN[i].Location = new Point(50, 50);
+                    pictureBTN[i].Image = Image.FromFile("../../images/vzaperti.jpg");
+                }
+                if (i == 1)
+                {
+                    pictureBTN[i].Location = new Point(350, 50);
+                    pictureBTN[i].Image = Image.FromFile("../../images/mi.jpg");
+                }
+                if (i == 2)
+                {
+                    pictureBTN[i].Location = new Point(650, 50);
+                    pictureBTN[i].Image = Image.FromFile("../../images/legenda.jpg");
+                }
+                pictureBTN[i].Click += MainForm_Click;
+                this.Controls.Add(pictureBTN[i]);
+            }
+            this.Text = "Кинотеатр Appolo";
+        }
+
+        private void MainForm_Click(object sender, EventArgs e)
+        {
+            Form1 form = new Form1();
+            form.ShowDialog();
+        }
+    }
+}
