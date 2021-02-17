f6827aeb1ee1c7344ed499f9fdd3a6ad4b28553e
Opilane TTHK <opilane@tthk.ee>
2021-02-17 15:48:01 +0200

    Cinema

--- Form1.cs
+++ Form1.cs
@@ -23,18 +23,19 @@ namespace kino
         int[] read_list, kohad_list;
         public int I = 0, J = 0;
         DateTimePicker datetime_;
-        ListBox saal;
+        ListBox saal, time;
         Button nextBtn;
         DataGridView data_;
         PictureBox poster;
+        Label[,] _arr;
+        Label[] read;
+        string zal,seans;
 
         public Form1(string selectedFilm)
         {
             InitializeComponent();
             film = selectedFilm;
         }
-        Label[,] _arr = new Label[4, 4];
-        Label[] read = new Label[4];
         Button osta;
         
         private void Osta_Click(object sender, EventArgs e)
@@ -43,33 +44,33 @@ namespace kino
                 "Appolo küsib", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
             if (vastus == DialogResult.Yes)
             {
-                for (int i = 0; i < 4; i++)
+                for (int i = 0; i < I; i++)
                 {
-                    for (int j = 0; j < 4; j++)
+                    for (int j = 0; j < J; j++)
                     {
                         if (_arr[i, j].BackColor == Color.LightGreen)
                         {
                             _arr[i, j].BackColor = Color.Transparent;
                             _arr[i, j].Image = Image.FromFile("../../images/red.png");
-                            this.Text = _arr[i, j].Name + read[i].Name;
-                            command = new SqlCommand("insert into client(client_name,film,seat,rjad) values(@n,@f,@s,@r)", connect);
                             connect.Open();
-                            command.Parameters.AddWithValue("@n", "name");
-                            command.Parameters.AddWithValue("@f", "film");
+                            command = new SqlCommand("INSERT INTO client(film,seat,rjad,zal,time,date) VALUES(@f,@s,@r,@z,@tm,@dt)", connect);
+                            command.Parameters.AddWithValue("@f", film);
                             command.Parameters.AddWithValue("@s", i);
                             command.Parameters.AddWithValue("@r", j);
+                            command.Parameters.AddWithValue("@z", saal.SelectedItem);
+                            command.Parameters.AddWithValue("@tm", time.SelectedItem);
+                            command.Parameters.AddWithValue("@dt", seans);
                             command.ExecuteNonQuery();
                             connect.Close();
-                            MessageBox.Show("Данные добавлены");
                         }
                     }
                 }
             }
             else
             {
-                for (int i = 0; i < 4; i++)
+                for (int i = I; i < I; i++)
                 {
-                    for (int j = 0; j < 4; j++)
+                    for (int j = J; j < J; j++)
                     {
                         if (_arr[i, j].BackColor == Color.LightGreen)
                         {
@@ -79,7 +80,6 @@ namespace kino
                     }
                 }
             }
-
         }
 
         void Form1_Click(object sender, EventArgs e)
@@ -97,51 +97,59 @@ namespace kino
             {
                 MessageBox.Show("Koht " + (tag[0] + 1) + (tag[1] + 1) + " juba ostetud!");
             }
-
-
         }
 
         private void seats_load()
         {
-            for (int i = I; i < I; i++)
+            this.Text = "Выбирете места";
+            _arr = new Label[I, J];
+            read = new Label[I];
+            osta = new Button();
+            osta.Text = "Osta";
+            osta.Location = new Point(J*10, I*55);
+            this.Controls.Add(osta);
+            osta.Click += Osta_Click;
+            for (int i = 0; i < I; i++)
             {
                 read[i] = new Label();
                 read[i].Text = "Rida " + (i + 1);
-                read[i].Size = new Size(100, 100);
-                read[i].Location = new Point(1, i * 100);
+                read[i].Size = new Size(50, 50);
+                read[i].Location = new Point(1, i * 50);
                 read[i].Name = $"rjad" + i;
                 this.Controls.Add(read[i]);
-                for (int j = J; j < J; j++)
+                for (int j = 0; j < J; j++)
                 {
                     _arr[i, j] = new Label();
-                    _arr[i, j].Size = new Size(100, 100);
+                    _arr[i, j].Size = new Size(50, 50);
                     _arr[i, j].Image = Image.FromFile("../../images/free2.png");
                     _arr[i, j].BorderStyle = BorderStyle.Fixed3D;
-                    _arr[i, j].Location = new Point(j * 100 + 100, i * 100);
+                    _arr[i, j].Location = new Point(j * 50 + 50, i * 50);
                     this.Controls.Add(_arr[i, j]);
                     _arr[i, j].Tag = new int[] { i, j };
                     _arr[i, j].Name = $"mesto" + j;
                     _arr[i, j].Click += new EventHandler(Form1_Click);
                 }
             }
-            osta = new Button();
-            osta.Text = "Osta";
-            osta.Location = new Point(500, 200);
-            this.Controls.Add(osta);
-            osta.Click += Osta_Click;
 
-            for (int i = 0; i < 4; i++)
+            
+
+            for (int i = 0; i < I; i++)
             {
-                for(int j = 0; j < 4; j++)
+                for(int j = 0; j < J; j++)
                 {
                     connect.Open();
-                    SqlCommand command = new SqlCommand("SELECT seat,rjad from client", connect);
+                    SqlCommand command = new SqlCommand("SELECT seat,rjad from client WHERE film = @f AND zal = @z AND date = @dt AND time = @tm", connect);
+                    command.Parameters.AddWithValue("@f", film);
+                    command.Parameters.AddWithValue("@z", zal);
+                    command.Parameters.AddWithValue("@dt", seans);
+                    command.Parameters.AddWithValue("@tm", time.SelectedItem);
                     using (SqlDataReader reader = command.ExecuteReader())
                     {
                         while (reader.Read())
                         {
                             if (Convert.ToInt32(reader["seat"]) == i && Convert.ToInt32(reader["rjad"]) == j)
                             {
+                                _arr[i, j].Text = "Kinni";
                                 _arr[i, j].Image = Image.FromFile("../../images/red.png");
                             }
                         }
@@ -155,12 +163,12 @@ namespace kino
         {
             this.Size = new Size(550,500);
             seats_load();
-            for (int i = 0; i < 4; i++)
+            for (int i = 0; i < I; i++)
             {
                 if(read[i] != null)
                 {
                     read[i].Hide();
-                    for (int j = 0; j < 4; j++)
+                    for (int j = 0; j < J; j++)
                     {
                         _arr[i, j].Hide();
                     }
@@ -171,7 +179,9 @@ namespace kino
             data_ = new DataGridView();
             data_.Hide();
 
-            for (int i = 0; i < 5; i++)
+            this.Text = "Информация о сеансе";
+
+            for (int i = 0; i < 4; i++)
             {
                 infoLabel[i] = new Label();
                 infoLabel[i].Size = new Size(150, 30);
@@ -192,21 +202,23 @@ namespace kino
                 {
                     infoLabel[i].Location = new Point(350, 120);
                 }
-                if (i == 4)
-                {
-                    infoLabel[i].Location = new Point(350, 150);
-                }
                 this.Controls.Add(infoLabel[i]);
             }
 
             datetime_ = new DateTimePicker();
             datetime_.Size = new Size(150,30);
             datetime_.Location = new Point(350,180);
+            datetime_.ValueChanged += Datetime__ValueChanged;
 
             saal = new ListBox();
-            saal.Size = new Size(150,150);
+            saal.Size = new Size(150,50);
             saal.Location = new Point(350,210);
             saal.SelectedIndexChanged += Saal_SelectedIndexChanged;
+
+            time = new ListBox();
+            time.Size = new Size(150, 50);
+            time.Location = new Point(350, 270);
+            time.Items.Add("Выбирете дату сеанса");
 
             nextBtn = new Button();
             nextBtn.Text = "Далее";
@@ -224,6 +236,7 @@ namespace kino
             Controls.Add(nextBtn);
             Controls.Add(data_);
             Controls.Add(poster);
+            Controls.Add(time);
 
 
             connect.Open();
@@ -252,6 +265,7 @@ namespace kino
             {
                 read_list[a] = (int)row["read"];
                 kohad_list[a] = (int)row["kohad"];
+                a += 1;
             }
 
             connect.Close();
@@ -274,30 +288,72 @@ namespace kino
 
             Id = Convert.ToInt32(data_.Rows[0].Cells[0].Value.ToString());
             infoLabel[2].Text = "Год: " + data_.Rows[0].Cells[2].Value.ToString();
-            infoLabel[3].Text = "Свободных мест: " + data_.Rows[0].Cells[3].Value.ToString();
-            infoLabel[4].Text = "Цена: " + data_.Rows[0].Cells[5].Value.ToString();
+            infoLabel[3].Text = "Цена: " + data_.Rows[0].Cells[5].Value.ToString();
+            seans = datetime_.Value.ToString("yyyy.MM.dd");
+        }
+
+        private void Datetime__ValueChanged(object sender, EventArgs e)
+        {
+            seans = datetime_.Value.ToString("yyyy.MM.dd");
         }
 
         private void Saal_SelectedIndexChanged(object sender, EventArgs e)
         {
             I = read_list[saal.SelectedIndex];
             J = kohad_list[saal.SelectedIndex];
-            this.Text = I.ToString();
+            zal = saal.SelectedItem.ToString();
+            if (datetime_.Value != null )
+            {
+                time.Items.Clear();
+
+                connect.Open();
+
+                DataTable table = new DataTable();
+                command = new SqlCommand("SELECT * FROM Time WHERE film=@name AND date=@d AND zal = @z", connect);
+                command.Parameters.AddWithValue("@name", film);
+                command.Parameters.AddWithValue("@d", seans);
+                command.Parameters.AddWithValue("@z", zal);
+
+                adapter = new SqlDataAdapter();
+                adapter.SelectCommand = command;
+                adapter.Fill(table);
+                if (table.Rows.Count > 0)
+                {
+                    foreach (DataRow row in table.Rows)
+                    {
+                        time.Items.Add(row["time"]);
+                    }
+                }
+                else
+                {
+                    time.Items.Add("Нет сеансов на этот день");
+                }
+
+                connect.Close();
+            }
         }
 
         private void NextBtn_Click(object sender, EventArgs e)
         {
-            this.Size = new Size(650,450);
-            for(int i = 0; i < 5; i++)
+            if(time.SelectedItem != null && saal.SelectedItem != null)
             {
-                infoLabel[i].Hide();
+                this.Size = new Size(650, 450);
+                for (int i = 0; i < 4; i++)
+                {
+                    infoLabel[i].Hide();
+                }
+                datetime_.Hide();
+                saal.Hide();
+                nextBtn.Hide();
+                data_.Hide();
+                poster.Hide();
+                time.Hide();
+                seats_load();
             }
-            datetime_.Hide();
-            saal.Hide();
-            nextBtn.Hide();
-            data_.Hide();
-            poster.Hide();
-            seats_load();
+            else
+            {
+                MessageBox.Show("Нужно выбрать время и зал");
+            }
         }
     }
 }
